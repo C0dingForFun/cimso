1515aeaef11f397b89af66addedf1ef8a5228846
C0dingForFun <gwibantulini8@gmail.com>
2024-11-04 09:18:48 +0200

    cimso

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    client:null
   },
   getters: {
   },
   mutations: {
+    setClients(state, payload){
+      state.client = payload
+    }
   },
   actions: {
+
   },
   modules: {
   }

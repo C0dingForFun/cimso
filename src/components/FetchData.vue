17a281d119cf3853a2b4d1e78bc6830608b50ee7
C0dingForFun <gwibantulini8@gmail.com>
2024-11-05 15:10:44 +0200

    cimso

--- src/components/FetchData.vue
+++ src/components/FetchData.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-      <h1>Unit Types</h1>
-      <button @click="show()">Show Unit Types</button>
-      <table class="table mt-2" v-show="check"> 
+      <h1>Welcome to CiMSO INNterchange</h1>
+      <button @click="showUnitType()">Show Unit Types</button> <button @click="showBookingUnit()">Show Booking Unit</button>
+      <table class="table mt-2" v-show="checkUnitType"> 
         <thead class="table-dark">
             <tr>
                 <th>Unit Type ID</th>
@@ -26,14 +26,23 @@
             </tr>
         </tbody>
       </table>
-      <!-- <ul>
-        <li v-for="unitType in unitTypes" :key="unitType['Unit Type ID']">
-          <h2>{{ unitType['Unit Type Description'] }} ({{ unitType['Unit Type Code'] }})</h2>
-          <p>Category: {{ unitType['Unit Type Category'] }}</p>
-          <p>Max Occupants: {{ unitType['Maximum Occupants'] }}</p>
-          <p>Marketing Description: {{ unitType['Marketing Description'] }}</p>
-          <p>Units Available: {{ unitType['Unit Count'] }}</p> -->
-  
+ 
+      <table class="table mt-2" v-show="checkBookingUnit"> 
+        <thead class="table-dark">
+            <tr>
+                <th>Booking Unit ID</th>
+                <th>Booking Unit Name</th>
+                <th>Booking Unit Number</th>
+            </tr>
+        </thead>
+        <tbody  v-for="unitType in unitTypes" :key="unitType['Unit Type ID']">
+            <tr v-for="unit in bookingUnits[unitType['Unit Type ID']] || []" :key="unit['Booking Unit ID']">
+                <td>{{ unit['Booking Unit ID']}}</td>
+                <td>{{ unit['Booking Unit Name'] }}</td>
+                <td>{{ unit['Booking Unit Number'] }}</td>
+            </tr>
+        </tbody>
+      </table>  
           <!-- <ul>
             <li v-for="unit in bookingUnits[unitType['Unit Type ID']] || []" :key="unit['Booking Unit ID']">
               <p>Booking Unit Name: {{ unit['Booking Unit Name'] }}</p>
@@ -55,12 +64,16 @@
       return {
         unitTypes: [],
         bookingUnits: {},
-        check:false
+        checkUnitType:false,
+        checkBookingUnit:false
       };
     },
     methods: {
-        show(){
-            this.check = !this.check
+        showUnitType(){
+            this.checkUnitType = !this.checkUnitType
+        },
+        showBookingUnit(){
+            this.checkBookingUnit = !this.checkBookingUnit
         }
     },
     async created() {
@@ -102,5 +115,13 @@
     font-size: 1.5em;
     margin-top: 1em;
   }
+    table td {
+        padding: 0.5rem;
+        border: 2px solid rgb(190, 215, 57);
+    }
+    table th {
+        padding: 0.5rem;
+        border: 2px solid rgb(190, 215, 57);
+    }
   </style>
   
